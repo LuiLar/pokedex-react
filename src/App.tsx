5a43a73208856753ea42f85ae5c52db0cdc7ebc8
Luis Larghi <windinglair@gmail.com>
2025-12-20 01:21:03 -0400

    Add tanstack-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { useEffect } from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 import PokemonDetails from "./components/PokemonDetails";
 import usePokemons from "./hooks/usePokemons";
+import { usePokemonStore } from "./utils/pokemonStore";
 
 function App() {
   const { pokemons } = usePokemons();
+  const { setPokemons, pokemons: storedPokemons } = usePokemonStore();
+
+  useEffect(() => setPokemons(pokemons), [pokemons]);
 
   return (
     <main>
-      {pokemons.map((pokemon) => (
+      {storedPokemons.map((pokemon) => (
         <PokemonCard key={pokemon.id} pokemon={pokemon} />
       ))}
       <PokemonDetails />
--- src/hooks/usePokemons.tsx
+++ src/hooks/usePokemons.tsx
@@ -1,38 +1,36 @@
 import { useEffect, useState } from "react";
 import { fetchPokemonByUrl, fetchPokemons } from "../services/pokeApi";
+import { useQuery } from "@tanstack/react-query";
 import { formatPokemonData } from "../utils/utils";
-import { usePokemonStore } from "../utils/pokemonStore";
 
 const usePokemons = () => {
-  const [pokeApiData, setPokeApiData] = useState<AllPokemonsApiResponse>({
-    results: [],
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  const { data, isSuccess } = useQuery({
+    queryKey: ["pokemons"],
+    queryFn: fetchPokemons,
   });
-  const pokemons = usePokemonStore((state) => state.pokemons);
-  const setPokemons = usePokemonStore((state) => state.setPokemons);
 
   useEffect(() => {
-    fetchPokemons()
-      .then((data) => setPokeApiData(data))
-      .catch((error) => console.error(error));
-  }, []);
+    if (isSuccess && data) {
+      const pokemonQueryPromises: Promise<PokemonApiResponse>[] = [];
 
-  useEffect(() => {
-    const resolvedPokemons: Pokemon[] = [];
-    const promises: Promise<PokemonApiResponse>[] = [];
+      data.results.map(({ url }) =>
+        pokemonQueryPromises.push(fetchPokemonByUrl(url)),
+      );
 
-    pokeApiData.results.forEach(({ url }) =>
-      promises.push(fetchPokemonByUrl(url)),
-    );
-
-    Promise.all(promises)
-      .then((pokemonData) => {
-        pokemonData.forEach((pokemon) =>
-          resolvedPokemons.push(formatPokemonData(pokemon)),
-        );
-      })
-      .catch((error) => console.error(error))
-      .finally(() => setPokemons(resolvedPokemons));
-  }, [pokeApiData, setPokemons]);
+      Promise.all(pokemonQueryPromises)
+        .then((pokemonData) => {
+          const formattedPokemons = pokemonData.map((data) =>
+            formatPokemonData(data),
+          );
+          setPokemons(formattedPokemons);
+        })
+        .catch((error) => {
+          console.error("Error fetching Pokémon data:", error);
+        });
+    }
+  }, [data, isSuccess]);
 
   return { pokemons };
 };
